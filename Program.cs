7235fff7fc92e1e07972e11b07009c4bf517b9b9
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-07-21 14:43:23 +0200

    Use UdtSharp as UDT library

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using System.Net.Sockets;
 using System.Collections.Generic;
 using System.Net;
-using System.IO;
+using UdtSharp;
 
 namespace p2pcopy
 {
@@ -63,7 +63,7 @@ namespace p2pcopy
 
                 ParseRemoteAddr(peer, out remoteIp, out remotePort);
 
-                Udt.Socket connection = PeerConnect(socket, remoteIp, remotePort);
+                UdtSocket connection = PeerConnect(socket, remoteIp, remotePort);
 
                 if (connection == null)
                 {
@@ -468,12 +468,12 @@ namespace p2pcopy
             return next - now.Second;
         }
 
-        static Udt.Socket PeerConnect(Socket socket, string remoteAddr, int remotePort)
+        static UdtSocket PeerConnect(Socket socket, string remoteAddr, int remotePort)
         {
             bool bConnected = false;
             int retry = 0;
 
-            Udt.Socket client = null;
+            UdtSocket client = null;
 
             while (!bConnected)
             {
@@ -493,16 +493,17 @@ namespace p2pcopy
                     if (client != null)
                         client.Close();
 
-                    client = new Udt.Socket(AddressFamily.InterNetwork, SocketType.Stream);
+                    client = new UdtSocket(AddressFamily.InterNetwork, SocketType.Stream);
 
-                    client.SetSocketOption(Udt.SocketOptionName.Rendezvous, true);
+                    //client.SetSocketOption(Udt.SocketOptionName.Rendezvous, true);
 
-                    client.Bind(socket);
+                    client.Bind(socket.LocalEndPoint as IPEndPoint);
 
                     Console.Write("\r{0} - Trying to connect to {1}:{2}.  ",
                         retry++, remoteAddr, remotePort);
 
-                    client.Connect(remoteAddr, remotePort);
+                    
+                    client.Connect(IPEndPoint.Parse(remoteAddr + ":" + remotePort.ToString()));
 
                     Console.WriteLine("Connected successfully to {0}:{1}",
                         remoteAddr, remotePort);
